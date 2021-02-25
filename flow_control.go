87f757c6cee9dfa8fdc05d26fd57f4d3e32c90a3
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-02-25 10:01:49 +0600

    1st commit

---
+++ flow_control.go
@@ -0,0 +1,89 @@
+package main
+
+import ("fmt")
+
+func main(){
+
+	//fmt.Print("Enter your age: ")
+	//var age int
+	//fmt.Scanf("%d", &age)
+
+	/* if else
+
+	if age < 3 {
+		fmt.Println("infant")
+
+	}else if age>2 &&age<13 {
+
+		fmt.Println("children")
+	}else if age>12 && age<19 {
+	
+		fmt.Println("teenage")
+	}else {
+	
+		fmt.Println("adult")
+	}
+	
+	*/
+
+
+//fixed value 
+/*
+	switch age {
+	case 2:
+		fmt.Println("infant")
+		fallthrough
+
+	case 3,4,5,6,7,8,9,10,11,12:
+		fmt.Println("children")
+
+	case 13,14,15,16,17,18,19:
+		fmt.Println("teenage")
+	default:
+		fmt.Println("adult")
+
+	}
+*/
+
+
+//for loop
+
+		/*	
+		for i:=1; i<=9; i++ {
+
+		fmt.Println(i)
+	}
+	*/
+
+	
+/*	
+students := []string{"Tareq", "Anonna", "Mainul"}
+
+	for i, std :=range students {
+	fmt.Println(i, std)
+
+	}
+	
+*/
+
+	i:=1 
+	for i<10{
+		fmt.Println(i,"hello")
+	i++
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	//fmt.Println(age)
+} 
